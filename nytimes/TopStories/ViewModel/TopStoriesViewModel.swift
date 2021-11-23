89865c0fd577e91bc010d93229a160299209e4d2
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2021-11-23 13:21:23 +0800

    Add setup with bookmark

--- nytimes/TopStories/ViewModel/TopStoriesViewModel.swift
+++ nytimes/TopStories/ViewModel/TopStoriesViewModel.swift
@@ -20,16 +20,37 @@ class TopStoriesViewModel {
             guard let self = self else { return }
             switch apiResponse.result {
             case .success(let data):
-                self.stories = data.stories
+                self.stories = data.stories?.filter({ !$0.title.isEmpty }) ?? []
                 viewController.activityIndicator.stopAnimating()
                 viewController.tableView.reloadData()
             case .failure(let error):
-                print(error.localizedDescription)
+                print("Errorrrrr: \(error)")
             }
         }
     }
     
+    func addOrRemoveBookmark(row: Int) {
+        guard let story = stories[safe: row] else { return }
+        guard !UserDefaults.bookmarkIds.contains(story.shortURL) else {
+            removeBookmark(row: row)
+            return
+        }
+        bookmark(row: row)
+    }
+    
     func story(in row: Int) -> Story? {
         return stories[safe: row]
+    }
+    
+    private func removeBookmark(row: Int) {
+        guard let story = stories[safe: row] else { return }
+        UserDefaults.bookmarks.removeAll(where: { $0.shortURL == story.shortURL })
+        UserDefaults.bookmarkIds.removeAll(where: { $0 == story.shortURL })
+    }
+    
+    private func bookmark(row: Int) {
+        guard let story = stories[safe: row] else { return }
+        UserDefaults.bookmarks.append(story)
+        UserDefaults.bookmarkIds.append(story.shortURL)
     }
 }
