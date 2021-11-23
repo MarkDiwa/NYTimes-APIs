bd11b01a1022b72268efc370012d6c5300a0a929
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2021-11-23 13:21:02 +0800

    Add saving of bookmarks

--- nytimes/Utils/Extensions.swift
+++ nytimes/Utils/Extensions.swift
@@ -7,10 +7,34 @@
 
 import Foundation
 
-
 extension Collection {
 
     subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
+    }
+}
+
+extension UserDefaults {
+    
+    static var bookmarks: [Story] {
+        get {
+            guard let data = UserDefaults.standard.data(forKey: "app.bookmarks") else { return [] }
+            let bookmark = try? JSONDecoder().decode([Story].self, from: data)
+            return bookmark ?? []
+        }
+        set {
+            UserDefaults.standard.set(try? JSONEncoder().encode(newValue), forKey: "app.bookmarks")
+        }
+    }
+    
+    static var bookmarkIds: [String] {
+        get {
+            guard let data = UserDefaults.standard.data(forKey: "app.ids") else { return [] }
+            let tempIds = try? JSONDecoder().decode([String].self, from: data)
+            return tempIds ?? []
+        }
+        set {
+            UserDefaults.standard.set(try? JSONEncoder().encode(newValue), forKey: "app.ids")
+        }
     }
 }
