f48db598c4d1e71b53ff488e33f250c2fecc1075
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2021-11-23 09:14:30 +0800

    Add Story Category enum

---
+++ nytimes/Networking/TopStories/StoryCategory.swift
@@ -0,0 +1,18 @@
+//
+//  StoryCategory.swift
+//  nytimes
+//
+//  Created by user204729 on 11/23/21.
+//
+
+import Foundation
+
+enum StoryCategory: String {
+    
+    case arts
+    case automobiles
+    case books
+    case business
+    case fashion
+    case food
+}
