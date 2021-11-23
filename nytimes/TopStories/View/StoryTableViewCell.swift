41812ae55c74e74bb6bde9302882a0dbd42644c1
Mark Dennis Diwa <markdennisdiwa@gmail.com>
2021-11-23 11:16:01 +0800

    Setup views and cells

--- nytimes/TopStories/View/StoryTableViewCell.swift
+++ nytimes/TopStories/View/StoryTableViewCell.swift
@@ -2,9 +2,10 @@
 //  StoryTableViewCell.swift
 //  nytimes
 //
-//  Created by user204729 on 11/23/21.
+//  Created by Mark Dennis Diwa on 11/23/21.
 //
 
+import Kingfisher
 import UIKit
 
 class StoryTableViewCell: UITableViewCell {
@@ -14,12 +15,26 @@ class StoryTableViewCell: UITableViewCell {
     @IBOutlet weak var publishedLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     
+    static let reuseIdentifier = "StoryTableViewCell"
+    static let nib = UINib(nibName: reuseIdentifier, bundle: nil)
+    
+    var storyViewModel: StoryCellViewModel?
+    var bookmarkButtonTapped: (StoryTableViewCell) -> Void = { _ in }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-    @IBAction func bookMarkButtonTapped(_ sender: Any) {
-        
+    func setup(with storyViewModel: StoryCellViewModel) {
+        self.storyViewModel = storyViewModel
+        titleLabel.text = storyViewModel.title
+        publishedLabel.text = storyViewModel.publishedDate
+        authorLabel.text = storyViewModel.author
+        storyViewModel.setImage(of: storyImage)
+    }
+    
+    @IBAction private func bookMarkButtonTapped(_ sender: Any) {
+        bookmarkButtonTapped(self)
     }
 }
--- nytimes/TopStories/View/TopStoriesViewController.swift
+++ nytimes/TopStories/View/TopStoriesViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 class TopStoriesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    let viewModel = TopStoriesViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        viewModel.getStories(in: self)
     }
 
+}
+
+extension TopStoriesViewController {
+    
+    func setup() {
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(StoryTableViewCell.nib,
+                           forCellReuseIdentifier: StoryTableViewCell.reuseIdentifier)
+    }
 }
 
 extension TopStoriesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: StoryTableViewCell.reuseIdentifier,
+                                                         for: indexPath)
+        guard let storyTableCell = dequeuedCell as? StoryTableViewCell,
+              let story = viewModel.story(in: indexPath.row) else { return dequeuedCell }
+        storyTableCell.setup(with: StoryCellViewModel(story: story))
         return UITableViewCell(frame: .zero)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel.numberOfItems
     }
     
 }
